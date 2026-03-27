fc05d481b36c148db798ee691819acbe0399c914
Oleksandr Dorokhov <alex.dorokhov.mail@gmail.com>
2026-03-27 11:02:11 +0200

    Implement Better-auth

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import "dotenv/config";
 import express from 'express';
 import subjectsRouter from './routes/subjects'
 import cors from 'cors'
-import { applySecurityPostBody, applySecurityPreBody, defaultLimiter } from './middleware/security';
+import { applySecurityPostBody, applySecurityPreBody, defaultLimiter } from './middleware/security'
+import {toNodeHandler} from 'better-auth/node'
+import { auth } from './lib/auth'
 
-const app = express();
+const app = express()
 const PORT = 8000;
 
 // CORS
@@ -18,30 +20,33 @@ app.use(cors({
   credentials: true
 }))
 
+// Better-auth
+app.all('/api/auth/*splat', toNodeHandler(auth))
+
 // Middleware
-applySecurityPreBody(app);
+applySecurityPreBody(app)
 
-app.use('/api/subjects', defaultLimiter);
+app.use('/api/subjects', defaultLimiter)
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
-applySecurityPostBody(app);
+applySecurityPostBody(app)
 
 // ROUTER
 app.use('/api/subjects', subjectsRouter)
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Dashboard API!' });
+  res.json({ message: 'Welcome to the Dashboard API!' })
 });
 
 // Start server
 const server = app.listen(PORT, () => {
 
 server.on('error', (error) => {
-  console.error('Error starting server:', error);
-  process.exit(1);
+  console.error('Error starting server:', error)
+  process.exit(1)
 });
-  console.log(`Server is running at http://localhost:${PORT}`);
+  console.log(`Server is running at http://localhost:${PORT}`)
 });
